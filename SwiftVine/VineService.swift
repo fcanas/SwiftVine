b47b50def372451739cdf5cf0ec8530ff6443fbe
Fabian Canas <fabian@mojotech.com>
2014-09-05 15:41:42 -0400

    Project structure

---
+++ SwiftVine/VineService.swift
@@ -0,0 +1,13 @@
+//
+//  VineService.swift
+//  SwiftVine
+//
+//  Created by Fabian Canas on 9/5/14.
+//  Copyright (c) 2014 Fabian Canas. All rights reserved.
+//
+
+import UIKit
+
+class VineService: NSObject {
+   
+}
